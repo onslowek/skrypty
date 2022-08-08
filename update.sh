6efa7abfa766fe926a62bb0f7457a8ba41a289bd
Michał <onslowek@gmail.com>
2022-08-08 08:16:42 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Skrypt do aktualizacji systemu i pakietow - odroid.pl
+echo 'Krok 1 - update'
+sudo apt update
+echo 'Krok 2 - full-upgrade'
+sudo apt full-upgrade -y
+echo 'Krok 3 - autoremove'
+sudo apt autoremove -y
+echo 'Krok 4 - clean'
+sudo rm -rf /var/cache/apt/archives/apt-fast
+sudo apt clean
+# echo 'Krok 5 - reboot'
+# sudo reboot

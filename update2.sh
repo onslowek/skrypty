8593f72b81e4978037932c76190885a9fb21f641
Michał <onslowek@gmail.com>
2022-08-16 14:31:29 +0200

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-read -n 1 -p "Wykonać full-upgrade czy upgrade?" ans;
+read -n 1 -p "Wykonać full-upgrade (f/F) czy upgrade (s/S)?" ans;
 
 case $ans in
     f|F)
-        echo '#1 update'
-        sudo apt update
-        echo '#2 upgrade'
-        sudo apt full-upgrade -y
-        echo '#3 autoremove'
-        sudo apt autoremove -y
-        echo '#4 clean'
-        sudo rm -rf /var/cache/apt/archives/apt-fast
+        echo '#1 update';;
+        sudo apt update;;
+        echo '#2 upgrade';;
+        sudo apt full-upgrade -y;;
+        echo '#3 autoremove';;
+        sudo apt autoremove -y;;
+        echo '#4 clean';;
+        sudo rm -rf /var/cache/apt/archives/apt-fast;;
         sudo apt clean;;
     s|S)
-        echo '#1 update'
-        sudo apt update
-        echo '#2 upgrade'
-        sudo apt upgrade -y
-        echo '#3 autoremove'
-        sudo apt autoremove -y
-        echo '#4 clean'
-        sudo rm -rf /var/cache/apt/archives/apt-fast
-        sudo apt clean
+        echo '#1 update';;
+        sudo apt update;;
+        echo '#2 upgrade';;
+        sudo apt upgrade -y;;
+        echo '#3 autoremove';;
+        sudo apt autoremove -y;;
+        echo '#4 clean';;
+        sudo rm -rf /var/cache/apt/archives/apt-fast;;
+        sudo apt clean;;
     *)
         exit;;
 esac

e26c1931615961f83e764eb28eca10666292f831
Michał <onslowek@gmail.com>
2022-08-16 12:46:52 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Skrypt do aktualizacji systemu i pakietow - odroid.pl
-echo 'Krok 1 - update'
+# Skrypt do aktualizacji systemu i pakietow - odroid.pl zmodyfikowany by onslowek
+echo '#1 update'
 sudo apt update
-echo 'Krok 2 - upgrade'
+echo '#2 upgrade'
 sudo apt upgrade -y
-echo 'Krok 3 - autoremove'
+echo '#3 autoremove'
 sudo apt autoremove -y
-echo 'Krok 4 - clean'
+echo '#4 clean'
 sudo rm -rf /var/cache/apt/archives/apt-fast
 sudo apt clean
-# echo 'Krok 5 - reboot'
+# echo '#5 reboot'
 # sudo reboot
